eb34d78254cee6f6ad1c9875bc53b94fd972be8f
jkortje750 <126585139+jkortje750@users.noreply.github.com>
2023-07-10 20:50:02 +0000

    My version of relatives parsing

--- src/11/Challenge/ch11_relatives.cpp
+++ src/11/Challenge/ch11_relatives.cpp
@@ -11,15 +11,20 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <unordered_map>
 
 // Finding Relatives, main()
 // Summary: This application displays the first pair of possible relatives from a list of names in a CSV file.
 int main(){
     std::fstream file ("names.csv", std::ios::in);
+    std::vector<std::string> fullnames;
     // Read the CSV file.
     if(file.is_open()){
-
+        std::string name;
         // Write your code here
+        while (std::getline(file, name, ',')) {
+            fullnames.push_back(name);
+        }
     
         file.close();
     }
@@ -29,8 +34,26 @@ int main(){
     }
 
     // Write your code here
-
-    std::cout << "No relatives found.\n\n" << std::flush;    
+    if (!fullnames.empty()) {
+        std::unordered_map<std::string, std::vector<std::string>> relatives;
+        for (auto name : fullnames) {
+            if (name.size() == 2) {
+                std::istringstream ss(name);
+                std::string firstname;
+                std::string lastname;
+                ss >> firstname >> lastname;
+                relatives[lastname].push_back(firstname);
+            }
+        }
+        for (auto it = relatives.begin(); it != relatives.end(); ++it) {
+            if (it->second.size() > 1) {
+                std::cout << it->second[0] << " " << it->first << " may be related to ";
+                std::cout << it->second[1] << " " << it->first << std::endl;
+            }
+        }
+    } else {
+        std::cout << "No relatives found.\n\n" << std::flush;
+    }    
     return 0;
 }
  
