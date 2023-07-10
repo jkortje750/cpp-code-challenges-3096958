578106a4937debcbb454b7fc7b04d4feb3b5a2f4
jkortje750 <126585139+jkortje750@users.noreply.github.com>
2023-07-10 23:01:13 +0000

    My version of Conway's Game of Life

--- src/13/Challenge/ch13_life.cpp
+++ src/13/Challenge/ch13_life.cpp
@@ -19,6 +19,42 @@
 #define N 10
 #define M 10
 
+// Summary: This function returns the number of live neighbors to a given cell.
+// Arguments:
+//           game[N][M]: The state of the game.
+//                    i: The cell's row.
+//                    j: The cell's column.
+// Returns: An integer with the live neighbor count.
+int live_neighbors(char game[][M],int i, int j){
+    int neighbors = 0; // counter
+    i += N; // yes, we increase it by the max dimension
+            // because we are going to take modulus later
+            // so this is a trick to handle wrapping over
+            // the edge of the grid
+    j += M;
+    // We use the modulous to check wrapping
+    // which then could be adjacent neighbors
+    if(game[(i-1)%N][(j-1)%M]=='X')
+        neighbors++;
+    if(game[(i-1)%N][(j)%M]=='X')
+        neighbors++;
+    if(game[(i-1)%N][(j+1)%M]=='X')
+        neighbors++;
+
+    if(game[(i)%N][(j-1)%M]=='X')
+        neighbors++;
+    if(game[(i)%N][(j+1)%M]=='X')
+        neighbors++;
+
+    if(game[(i+1)%N][(j-1)%M]=='X')
+        neighbors++;
+    if(game[(i+1)%N][(j)%M]=='X')
+        neighbors++;
+    if(game[(i+1)%N][(j+1)%M]=='X')
+        neighbors++;
+    return neighbors;
+}
+
 // Conway's Game of Life, main()
 // Summary: This application is a simulation of Conway's game of life.
 int main(){    
@@ -39,6 +75,8 @@ int main(){
     game[3][8]='X'; // - - - - - - X X X -
     
     int generation = 0;
+    char cloned_game[N][M];
+
     std::string go_on;
     do{
         std::cout << "Generation #" << generation++ << "\n";
@@ -50,12 +88,39 @@ int main(){
         std::cout << "\n";
 
         // Write your code here
+        std::memcpy(cloned_game,game,N*M);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                int n = live_neighbors(game, i,  j);
+                // Any live cell with fewer than two live neighbors dies
+                // as if by underpopulation
+                if ((game[i][j] == 'X') && (n < 2)) {
+                    cloned_game[i][j] = '-';
+                }
+                // Any live cell with two or three live neighbors lives
+                // on to the next generation
+                if ((game[i][j] == 'X') && ((n == 2) || (n == 3))) {
+                   cloned_game[i][j] = 'X';
+                }
+                // Any live cell with more than three live neighbors dies
+                // as if by overpopulation
+                if ((game[i][j] == 'X') && (n > 3)) {
+                    cloned_game[i][j] = '-';
+                }
+                // Any dead cell with exactly three live neighbors becomes a live cell,
+                // as if by reproduction.
+                if ((game[i][j]=='-') && (n==3)) {
+                    cloned_game[i][j]='X';
+                }
+            }
+        }
+        std::memcpy(game, cloned_game, N*M);
         
         std::cout << "Press Enter for the next generation, or type \"Exit\": " << std::flush;
         std::getline(std::cin,go_on);
         std::transform(go_on.begin(), go_on.end(), go_on.begin(), toupper);
         std::cout << "\n" << std::flush;
-    }while(go_on.compare("EXIT")!=0);
+    } while(go_on.compare("EXIT")!=0);
     
     return 0;
 }
